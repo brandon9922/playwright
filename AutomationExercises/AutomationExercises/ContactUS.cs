a51805a5600a8e2c3ddc27474d314f2c501b6029
Pawel <p.sarnowski99@gmail.com>
2025-07-17 00:07:54 -0700

    contact form update

--- AutomationExercises/AutomationExercises/ContactUS.cs
+++ AutomationExercises/AutomationExercises/ContactUS.cs
@@ -17,23 +17,24 @@ public class ContactUsFrom: PageTest
         await Page.Locator("[data-qa='name']").FillAsync("test");
         await Page.Locator("[data-qa='email']").FillAsync("k.sarnowska99@gmail.com");
         await Page.Locator("[data-qa='subject']").FillAsync("Testqwerty");
-        await Page.Locator("[data-qa='message']").FillAsync(@"Lorem Ipsum is simply 
-                dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                 and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
-        await Page.Locator("input[type='file']").SetInputFilesAsync("C:\\Users\\pawel\\Desktop\\test photos\\test55.jpg");
-        //await Page.PauseAsync();
-        await Page.Locator("[data-qa='submit-button']").ClickAsync();
+        await Page.Locator("[data-qa='message']").FillAsync(@"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+
+        var fileChooser = await Page.RunAndWaitForFileChooserAsync(async () =>
+        {
+            await Page.Locator("input[type='file']").ClickAsync();
+
+        });
+        await fileChooser.SetFilesAsync("C:\\Users\\pawel\\Desktop\\test photos\\test55.jpg");
+        
         Page.Dialog += async (_, dialog) =>
         {
             await dialog.AcceptAsync();
         };
-        await Page.PauseAsync();
-        await Expect(Page.GetByText("Get In Touch")).ToBeVisibleAsync();
-        //await Expect (Page.Locator("div:has-text('Success! Your details have been submitted successfully.')")).ToBeVisibleAsync();
-
+        await Page.Locator("[data-qa='submit-button']").ClickAsync();
+        
+        //await Expect(Page.GetByText("Success! Your details have been submitted successfully.")).ToBeVisibleAsync(); nie działa
+        
+        await Expect(Page.Locator(".contact-form").GetByText("Success! Your details have been submitted successfully.")).ToBeVisibleAsync();
+        
     }
 }
